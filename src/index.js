b3fca6a64a5d79753ed197e8b7e71dada462c9e6
Alexandre <3979539+alxblog@users.noreply.github.com>
2020-12-05 23:49:12 +0100

    Add Command route

--- src/index.js
+++ src/index.js
@@ -7,19 +7,24 @@ const fileUpload = require("express-fileupload")
 const throttle = require('express-throttle-bandwidth')
 const chalk = require('chalk')
 const Table = require('cli-table')
+const { init } = require('./init')
+const { sendSensorData, SendBinary, sendBinary } = require('./lib')
+const ESP3D = require('./lib/esp3d')
+const {
+    colorizedHTTPMethods,
+} = require('./lib/dev')
 
 /**
  * Ext Routes
  */
 var filesRoute = require('./routes/files')
+var commandRoute = require('./routes/command')
 
-// ...
 
-const throttleValue = 0//100000 // bps // if bps is <= 0 it does not throttle.
-const port = 8888
-const wsPort = 8830
+const throttleValue = 100000//100000 // bps // if bps is <= 0 it does not throttle.
+const port = 8080 //8888
+const wsPort = 81//8830
 const fsDir = "public"
-const firmware = process.env.FIRMWARE || "marlin"
 let currentID = 0
 let sensorInterval = null
 let tempInterval = null
@@ -28,6 +33,7 @@ let feedrate = 100
 
 const app = express()
 const wss = new WebSocket.Server({ port: wsPort })
+const esp3d = new ESP3D(process.env.FIRMWARE || "marlin", wss)
 
 /**
  * MIDDLEWARES
@@ -39,13 +45,15 @@ app.use(fileUpload({ preserveExtension: true, debug: false }))
 
 app.use((req, res, next) => {
     // console.log('Time: ', Date.now())
-    const table = new Table();
+    const table = new Table()
+    const [service, args] = req.originalUrl.split('?')
     table.push(
-        ['Method', chalk`{black.bgWhite ${req.method}}`],
-        ['URL', req.originalUrl],
-        ['Query params', JSON.stringify(req.query)]
+        ['Firmware', chalk`{cyan ${esp3d.targetFW}}`],
+        ['Method', colorizedHTTPMethods[req.method]],
+        ['URL', chalk`{magenta ${service}}?${args}`],
+        ['Query params', JSON.stringify(req.query, null, 2)]
         //req.params
-    );
+    )
     console.log(table.toString())
     next()
 })
@@ -57,19 +65,21 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-app.post("/login", function (req, res) {
-    // res.send("")
-    // return
-})
-
+app.post("/login", (req, res) => { res.send("") })
 app.use('/files', filesRoute(fsDir))
+app.use('/command', commandRoute(esp3d))
 
 app.all("/updatefw", function (req, res) { res.send("ok") })
 
-app.listen(process.env.PORT || port, () => {
-    console.log(chalk`{white.bgBlack ESP}{black.bgWhite 3D} Mocking Server listening at {cyan http://localhost:${process.env.PORT || port}}`)
-    console.log(chalk`Firmware : {cyan.bold ${firmware}}`)
+const run = (async () => {
+    await init()
+    app.listen(process.env.PORT || port, () => {
+        console.log(chalk`{white.bgBlack ESP}{black.bgWhite 3D} Mocking Server listening at {cyan http://localhost:${process.env.PORT || port}}`)
+        console.log(chalk`Firmware : {cyan.bold ${esp3d.targetFW}}`)
+    })
 })
+
+run()
 
 wss.on("connection", ws => {
     console.log("WS : New connection")
---
+++ src/routes/command.js
@@ -0,0 +1,122 @@
+const express = require('express')
+const router = express.Router()
+const grbl = require('../mocked-responses/grbl')
+const esp800 = require('../mocked-responses/ESP800')
+const esp400 = require('../mocked-responses/ESP400')
+const esp401 = require('../mocked-responses/ESP401')
+const esp410 = require('../mocked-responses/ESP410.json')
+const esp420 = require('../mocked-responses/ESP420.json')
+const smCat = require('../mocked-responses/smoothieware')
+const m205 = require('../mocked-responses/M205.repetier')
+const m114 = require('../mocked-responses/M114')
+const m503 = require('../mocked-responses/M503')
+
+const {
+    extractGcodeCmd,
+    sendSensorData,
+} = require('../lib')
+
+const commandRoute = (esp3d) => {
+    const targetFW = esp3d.targetFW
+    router.all("/", (req, res) => {
+        const { cmd } = req.query
+        const [command, ...rest] = decodeURI(cmd).split(' ')
+        const extratedCmd = extractGcodeCmd(command) || command  //extract command ex: M500 or raw command
+        switch (extratedCmd) {
+            case "$$": //GRBL Settings
+                if (targetFW == "grbl") esp3d.sendBinary(grbl)
+                res.send("ok")
+                break;
+            case "cat": //Smoothieware Settings
+                if (targetFW == "smoothieware") esp3d.sendBinary(smCat())
+                res.send("ok")
+                break;
+            case "ESP400": //Get full EEPROM settings content
+                res.json(esp400(esp3d.getFWId(targetFW)))
+                break;
+            case "ESP401": //Set EEPROM setting //TO-CHECK BY LUC
+                esp401(rest, esp3d)
+                res.send("ok ")
+                break;
+            case "ESP410": //Get available AP list (limited to 30)
+                res.send(esp410)
+                break;
+            case "ESP420": // Get current settings of ESP3D
+                res.json(esp420)
+                break;
+            case "ESP800": //Get FW Informations
+                res.json(esp800(targetFW))
+                break;
+            case "M20":
+                res.send(rest)
+                break;
+            case "M21":
+                if (["repetier", "repetier4davinci"].includes(targetFW))
+                    esp3d.sendBinary("ok\n");
+                res.send("")
+                break;
+            case "M105":
+                break;
+            case "M114":
+                esp3d.sendBinary(m114())
+                res.send("")
+                break;
+            case "M205":
+                if (targetFW == "repetier") esp3d.sendBinary(m205)
+                res.send("")
+                break;
+            case "M220":
+                break;
+            case "M408":
+                break;
+            case "M503":
+                esp3d.sendBinary(m503(targetFW))
+                res.send("")
+                break;
+            default:
+                res.json({ custom: "unknown query" })
+                // SendBinary("ok\n")
+                break;
+        }
+
+    })
+    // HANDLE COMMANDS HERE
+    return router
+}
+
+module.exports = commandRoute
+
+/*
+ECHO%3A
+ECHO%3A
+AUTOWAIT
+ON
+OFF
+M20%20SD%3A
+M20%20U%3A
+M20
+M21
+M105
+M105%20ON
+M105%20OFF
+M114
+M205
+M220
+M220%20S
+M220%20S
+M408
+M503
+ESP420
+ESP400
+ESP401
+ESP410
+ESP800
+SENSOR%20ON
+SENSOR%20OFF
+=%24%24
+=cat
+P%3D
+%20T%3D
+V%3D
+&
+*/
